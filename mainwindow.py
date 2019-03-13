978c8fbb13a92ae425dc512e9191183d62faf9a9
o-danilo <oliveira.danilo94@gmail.com>
2019-03-13 20:42:41 -0300

    implementado connects dos botões

--- mainwindow.py
+++ mainwindow.py
@@ -2,10 +2,20 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from ui_mainwindow import Ui_MainWindow
 
+def showWindowPessoa():
+    print("olá")
+
+def showWindowEspacoFisico():
+    print("oi!")
+
+
 app = QApplication(sys.argv)
 window = QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(window)
+
+ui.btnPeople.clicked.connect(showWindowPessoa)
+ui.btnPlaces.clicked.connect(showWindowEspacoFisico)
 
 window.show()
 sys.exit(app.exec_())
